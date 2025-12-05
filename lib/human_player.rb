9a8db3326a034e1924653e53f80c5494d9876077
gbntb <228612152+gbntb@users.noreply.github.com>
2025-12-05 11:58:23 -0400

    Make #input_combination return the combination

--- lib/human_player.rb
+++ lib/human_player.rb
@@ -4,12 +4,14 @@
 # or create a combination of his own to have the computer guess.
 class HumanPlayer < Player
   def input_combination(colors)
+    print 'Your colors: '
     combination_valid = false
 
     until combination_valid
       combination = gets.chomp.split(', ')
       combination_valid = combination_colors_valid?(combination, colors) && combination_colors_uniq?(combination)
     end
+    combination
   end
 
   private
