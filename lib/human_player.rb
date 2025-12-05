28f558a0ebbf8a6aede2631007ee624326fa17e0
gbntb <228612152+gbntb@users.noreply.github.com>
2025-12-05 11:00:37 -0400

    Start combination input in HumanPlayer

---
+++ lib/human_player.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+# Human player, can try to guess the computer's secret combination,
+# or create a combination of his own to have the computer guess.
+class HumanPlayer < Player
+  def input_combination(colors)
+    combination_valid = false
+
+    until combination_valid
+      combination = gets.chomp.split(', ')
+      combination_valid = combination_colors_valid?(combination, colors) && combination_colors_uniq?(combination)
+    end
+  end
+
+  private
+
+  def combination_colors_uniq?(combination)
+    combination.uniq.length == 4
+  end
+
+  def combination_colors_valid?(combination, colors)
+    combination.all? { |color| colors.include?(color) }
+  end
+end
